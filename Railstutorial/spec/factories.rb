c0ffc5b4b203b53fb31226cfcf77077158298283
Gyergyói Dávid Ferenc <gyedafe@DAVVBOOK.(none)>
2011-01-07 09:26:53 +0100

    Done with user edit/update, index, and destroy actions

--- Railstutorial/spec/factories.rb
+++ Railstutorial/spec/factories.rb
@@ -1,6 +1,11 @@
 Factory.define :user do |user|
   user.name                  "Michael Hartl"
-  user.mail                  "mhartl@example.com"
+  user.mail                 "mhartl@example.com"
   user.password              "foobar"
   user.password_confirmation "foobar"
 end
+
+Factory.sequence :mail do |n|
+  "person-#{n}@example.com"
+end
+
